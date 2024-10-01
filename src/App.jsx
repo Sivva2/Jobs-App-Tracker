e6d7f5a16ddf21e7105ca92e1ca0040f9120fbee
Kevin Abaskaran <shiva94460@gmail.com>
2024-10-01 18:47:42 +0200

    JobList fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import UpdateJob from "./Pages/UpdateJob";
 import JobDetailPage from "./Pages/JobDetailsPage";
 import HomePage from "./Pages/HomePage";
 import ErrorPage from "./Pages/ErrorPage";
-/* import JobList from "./Pages/JobList"; */
+import JobList from "./Pages/JobList";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        {/* <Route path="/job" element={<JobList />} /> */}
+        {<Route path="/job" element={<JobList />} />}
         <Route path="/" element={<HomePage />} />
         <Route path="/job/:jobId" element={<JobDetailPage />} />
         <Route path="*" element={<ErrorPage />} />
