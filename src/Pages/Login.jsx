e006b33324b71a3d36c8438b610dca21febca6a8
alexandrolastra <alexandrolastra.fr@gmail.com>
2024-10-03 18:51:41 +0200

    alexandro merge Login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -6,10 +6,8 @@ import {
   TextInput,
   PasswordInput,
   Button,
-  Autocomplete,
   Fieldset,
   Checkbox,
-  VisuallyHidden,
 } from "@mantine/core";
 import classes from "../styles/Home.module.css";
 
@@ -76,29 +74,3 @@ const Login = () => {
   );
 };
 export default Login;
-
-/* 
-export default Login;
-import React, { useState } from "react";
-import { Box, Title, Text, TextInput, PasswordInput, Button } from "@mantine/core";
-import classes from "../styles/Home.module.css"; 
-
-  return (
-    <Box className={classes.home}>
-      <Title className={classes.title}>Welcome to Jobs App Tracker</Title>
-      <Box className={classes.paragraphBox}>
-        <Text className={classes.paragraph}>
-          This application helps you keep track of your job applications, manage
-          your job search process, and stay organized.
-        </Text>
-        <Text className={classes.paragraph}>
-          You can add new job listings, update existing ones, and monitor your
-          progress. Stay on top of your job search with Jobs App Tracker!
-        </Text>
-      </Box>
-    </Box>
-  );
-};
-
-export default Home;
- */
